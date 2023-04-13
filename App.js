0410a4f638733f14157975bf0f338c4b83d95de7
Bruno Lima <lima14032001@gmail.com>
2023-04-13 16:28:39 -0300

    onde inicializamos os projetos

---
+++ App.js
@@ -0,0 +1,16 @@
+// import HelloWorld1 from './components/HelloWorld1'
+//import HelloWorld2 from './components/HelloWorld2'
+//import FacebookLogin from "./components/FacebookLogin";
+//import TheLight from './components/TheLight'
+//import SignUp from './components/SignUp'
+import Instagram from './components/Instagram'
+
+export default function App(){
+
+  // return <HelloWorld1/>
+  // return <HelloWorld2/>
+  // return <TheLight/>
+  //return <FacebookLogin/>
+  //return <SignUp/>
+  return <Instagram/>
+} 
